043e756eea47bf535f53b0d04fd37719954f0718
Steve Falcigno <steve1281@hotmail.com>
2025-07-09 15:18:27 -0400

    add in LoadImage and a Window_Loaded and tune up the form a bit

--- ApiConsumerDemo/MainWindow.xaml.cs
+++ ApiConsumerDemo/MainWindow.xaml.cs
@@ -17,11 +17,30 @@ namespace ApiConsumerDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int maxNumber;
+        private int currentNumber;
+
         public MainWindow()
         {
             InitializeComponent();
             ApiHelper.InitializeClient();
 
+        }
+        private async Task LoadImage(int imageNumber =0)
+        {
+            var comic = await (new ComicProcessor()).LoadComic(imageNumber);
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+            currentNumber = comic.Num;
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSource);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImage();
         }
     }
 }
