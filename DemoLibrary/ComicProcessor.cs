9054de1143fb0bfd381cdcc309c23509a70351e9
Steve Falcigno <steve1281@hotmail.com>
2025-07-09 14:51:47 -0400

    add call to xkcd and a model to handle returned data

---
+++ DemoLibrary/ComicProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ComicProcessor
+    {
+
+        public async Task<ComicModel> LoadComic(int comicNumber=0)
+        {
+            // 0 will equate to the current comic
+            string url = "";
+
+            if (comicNumber > 0)
+            {
+                url = $"https://xkcd.com/{comicNumber}/info.0.json";
+            } else
+            {
+
+                url = $"https://xkcd.com/info.0.json";
+            }
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode) 
+                {
+                    ComicModel comic = await response.Content.ReadAsAsync<ComicModel>();
+
+                    return comic;
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+
+        }
+    }
+}
