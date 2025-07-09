4280e72794ab5026215afce420e1bbcbbef7992a
Steve Falcigno <steve1281@hotmail.com>
2025-07-09 14:23:12 -0400

    add code to initialize our apiclient

--- ApiConsumerDemo/MainWindow.xaml.cs
+++ ApiConsumerDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using DemoLibrary;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace ApiConsumerDemo
         public MainWindow()
         {
             InitializeComponent();
+            ApiHelper.InitializeClient();
+
         }
     }
 }
